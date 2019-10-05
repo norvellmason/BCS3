f1cea4f78f494d7185c292cfa02e82ef7c15762d
Mason Norvell <norvellmason@gmail.com>
2019-10-05 10:59:47 -0600

    Added GameState class

---
+++ BCS 3/GameState.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCS_3
+{
+    abstract class GameState
+    {
+        protected SpriteBatch SpriteBatch;
+        protected GraphicsDevice GraphicsDevice;
+
+        public GameState(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch)
+        {
+            this.GraphicsDevice = graphicsDevice;
+            this.SpriteBatch = spriteBatch;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected virtual void Initialize()
+        {
+            // TODO: Add your initialization logic here
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected virtual void LoadContent()
+        {
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected virtual void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public virtual void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public virtual void Draw(GameTime gameTime)
+        {
+            // TODO: Add your drawing code here
+        }
+    }
+}
