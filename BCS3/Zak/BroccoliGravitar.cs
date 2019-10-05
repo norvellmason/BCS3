e1ad8aaa81e9acdd8a173f2b63270d76c5d34f47
ztstroud <ztstroud@gmail.com>
2019-10-05 14:18:10 -0600

    Initial Commit

---
+++ BCS3/Zak/BroccoliGravitar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BCS_3
+{
+    public class BroccoliGravitar : GameState
+    {
+        public BroccoliGravitar(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch)
+            : base(graphicsDevice, spriteBatch)
+        {
+
+        }
+
+        protected override void LoadContent()
+        {
+            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            
+        }
+    }
+}
