bf7ed7e02c2da73cc1dbbf84f654de81289acb4a
ztstroud <ztstroud@gmail.com>
2019-10-05 14:38:45 -0600

    Add test for graphics

--- BCS3/Zak/BroccoliGravitar.cs
+++ BCS3/Zak/BroccoliGravitar.cs
@@ -6,20 +6,23 @@ using System.Threading.Tasks;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
 
 namespace BCS_3
 {
     public class BroccoliGravitar : GameState
     {
-        public BroccoliGravitar(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch)
-            : base(graphicsDevice, spriteBatch)
+        private Texture2D arrow;
+
+        public BroccoliGravitar(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch, ContentManager content)
+            : base(graphicsDevice, spriteBatch, content)
         {
 
         }
 
-        protected override void LoadContent()
+        protected override void LoadContent(ContentManager content)
         {
-            
+            arrow = content.Load<Texture2D>("Zak/arrow");
         }
 
         public override void Update(GameTime gameTime)
@@ -29,7 +32,11 @@ namespace BCS_3
 
         public override void Draw(GameTime gameTime)
         {
-            
+            SpriteBatch.Begin();
+
+            SpriteBatch.Draw(arrow, new Vector2(100, 100), Color.White);
+
+            SpriteBatch.End();
         }
     }
 }
