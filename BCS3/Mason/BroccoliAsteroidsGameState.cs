ab7e0aa6d1a2d345a841eb3b9f32fe3295c3ddce
Mason Norvell <norvellmason@gmail.com>
2019-10-05 16:24:09 -0600

    Added racecar.

--- BCS3/Mason/BroccoliAsteroidsGameState.cs
+++ BCS3/Mason/BroccoliAsteroidsGameState.cs
@@ -11,26 +11,31 @@ namespace BCS_3.Mason
 {
     class BroccoliAsteroidsGameState : GameState
     {
+        private RaceCar raceCar;
+        private Texture2D carTexture;
+
         public BroccoliAsteroidsGameState(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch, ContentManager contentManager) : base(graphicsDevice, spriteBatch, contentManager)
         {
-
+            this.raceCar = new RaceCar(carTexture, 0.1f);
         }
 
         protected override void LoadContent(ContentManager contentManager)
         {
+            contentManager.Load<Texture2D>("Mason/car");
+
             base.LoadContent(contentManager);
         }
 
         public override void Update(GameTime gameTime)
         {
-
+            
 
             base.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
-            
+            this.raceCar.Draw(gameTime, SpriteBatch);
         }
     }
 }
